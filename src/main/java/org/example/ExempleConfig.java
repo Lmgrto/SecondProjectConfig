acd3a9cc0184c09300f9872351c1eea5a61f7ffd
Lmgrto <leo.mangaretto@gmail.com>
2024-02-05 11:28:43 -0500

    Initial commit

---
+++ src/main/java/org/example/ExempleConfig.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.List;
+import java.util.Map;
+
+public class ExempleConfig {
+
+    private Map<String, List<ObjectTest>> list;
+
+    public Map<String, List<ObjectTest>> getList() {
+        return list;
+    }
+
+    public void setList(Map<String, List<ObjectTest>> list) {
+        this.list = list;
+    }
+}
---
+++ src/main/java/org/example/ObjectTest.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.util.Objects;
+
+public class ObjectTest {
+    private String name;
+    private String type;
+
+    public ObjectTest(String name, String type) {
+        super();
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ObjectTest that = (ObjectTest) o;
+        return Objects.equals(name, that.name) && Objects.equals(type, that.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, type);
+    }
+}
